5791857ac9bc476e75b6334f3445771c051adc2b
Santiago Padron <santiagopadron315@gmail.com>
2025-11-19 20:16:56 -0500

    CITA

--- tareas de lab_prog_2/tarea 3/citas/CITA.cpp
+++ tareas de lab_prog_2/tarea 3/citas/CITA.cpp
@@ -4,25 +4,5 @@
 
 Cita::Cita() { memset(&r,0,sizeof(r)); r.id=0; r.atendida=false; r.eliminado=false; r.consultaID=-1; }
 const CitaRecord& Cita::toRecord() const { return r; }
-void Cita::fromRecord(const CitaRecord& rec) { r = rec; }
-```
-
-
----
-
-
-### File: citas/operacionesCitas.hpp
-```cpp
-#ifndef OPERACIONES_CITAS_HPP
-#define OPERACIONES_CITAS_HPP
-
-
-#include "Cita.hpp"
-#include "../hospital/Hospital.hpp"
-
-
-namespace OperCitas {
-bool agendarCita(Hospital& h);
-bool cancelarCita(int id);
-bool atenderCita(Hospital& h, int id);
+void Cita::fromRecord(const CitaRecord& rec) { r = rec; 
 }
